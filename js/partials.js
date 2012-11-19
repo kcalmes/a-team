770adcf74703dec9dcf24bf52a9e8e2ed26c5f78
Kory Calmes <kory@calmes.org>
2012-11-19 15:11:17 -0700

    Added a light weight js framework.

---
+++ js/partials.js
@@ -0,0 +1,9 @@
+$(function(){  
+  navigate();
+  window.onhashchange = navigate;
+});
+
+function navigate(){
+  var page = document.location.hash.substr(1);
+  $('#main-content').html(new EJS({url: './pages/'+page+'.ejs'}).render({}));
+}
