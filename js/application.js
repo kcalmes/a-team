123bb0431e7bdaea5d8d47e42ccd89e3176f3efa
Kory Calmes <kory@calmes.org>
2012-11-30 20:06:52 -0700

    Made menus work.
    Right now just 3 work for student side, but will be refining later.  There is also an initial load bug, will be working on that too.

---
+++ js/application.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  //set click listener to show menus
+  $('#subnav h5').click(function(event){
+    menu = $(this).parents('.menu');
+    if(menu.css('margin-top') == '0px'){
+      menu.animate( {'margin-top': '-'+menu.find('ul').outerHeight(true)+'px'}, 200, 'swing');
+    } else {
+      $('#subnav h5').each(function(){
+        $(this).parents('.menu').animate( {'margin-top': '-'+$(this).parents('.menu').find('ul').outerHeight(true)+'px'}, 200, 'swing');
+      });
+      menu.animate( {'margin-top': '0px'}, 200, 'swing');
+    }
+    /*
+    $('#subnav .menu').css('margin-top','-'+$('#subnav .menu ul').outerHeight(true)+'px');
+    .animate( properties [, duration] [, easing] [, complete] )
+    //$('#subnav ul.menu').slideDown();
+    */
+  });
+  $('#subnav h5').each(function(){
+    $(this).trigger('click');
+  });
+});
+
+//24 - 16 - 11
+
+//1250 + 1000
