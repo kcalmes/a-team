f333adc98b63ab63598f79dc4de8ac6455a1c631
Kory Calmes <kory@calmes.org>
2012-12-04 08:14:01 -0700

    Improved menus and fleshed out structure.

--- js/partials.js
+++ js/partials.js
@@ -5,6 +5,16 @@ $(function(){
 
 function navigate(){
   var page = document.location.hash.substr(1);
+  var scope = page.substr(0,page.indexOf('/'));
+  if(scope == "students"){
+    $('#subnav .students').addClass('hide');
+    $('#subnav .advisors').addClass('hide');
+    $('#subnav .students').removeClass('hide');
+  } else if(scope == "advisors"){
+    $('#subnav .students').addClass('hide');
+    $('#subnav .advisors').addClass('hide');
+    $('#subnav .advisors').removeClass('hide');
+  }
   if(page != ""){
     $('#main-content').html(new EJS({url: './pages/'+page+'.ejs'}).render({}));
   }
