c1bbc62a14b16250ac084d140147b8f1d7641f2c
Kory Calmes <kory@calmes.org>
2012-12-04 21:43:29 -0700

    Configured menu system and linked up most advisor pages.  Student pages didn't exist yet.
    Didn't connect searching resource pages.  Didn't know how that was setup.

--- js/partials.js
+++ js/partials.js
@@ -5,15 +5,27 @@ $(function(){
 
 function navigate(){
   var page = document.location.hash.substr(1);
-  var scope = page.substr(0,page.indexOf('/'));
+  var scope = page.substr(0,page.lastIndexOf('/'));
+  console.log(scope);
+  //set the buttons
+  $('#header .row').each(function(){
+    $(this).addClass('hide');
+  });
+  
+  //set all of the nav to hidden
+  $('#subnav').each(function(){
+    $(this).find('.row').addClass('hide');
+  });
+  //set the menu to be used
   if(scope == "students"){
-    $('#subnav .students').addClass('hide');
-    $('#subnav .advisors').addClass('hide');
-    $('#subnav .students').removeClass('hide');
-  } else if(scope == "advisors"){
-    $('#subnav .students').addClass('hide');
-    $('#subnav .advisors').addClass('hide');
-    $('#subnav .advisors').removeClass('hide');
+    $('#header #student-buttons').removeClass('hide');
+    $('#subnav #students-advisement').removeClass('hide');
+  } else if(scope == "advisors/students" || page == "advisors/students" || page == "advisors"){
+    $('#header #advisor-buttons').removeClass('hide');
+    $('#subnav #advisors-students').removeClass('hide');
+  } else if(scope == "advisors/resources" || page == "advisors/resources"){
+    $('#header #advisor-buttons').removeClass('hide');
+    $('#subnav #advisors-resources').removeClass('hide');
   }
   if(page != ""){
     $('#main-content').html(new EJS({url: './pages/'+page+'.ejs'}).render({}));
